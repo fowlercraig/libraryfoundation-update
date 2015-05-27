704bcfd7da9e4452ee924d0573af7e08b22a04ec
Craig Fowler <craig@wearegiants.com>
2015-05-27 15:23:06 -0700

    newest things.

--- templates/media/newest.php
+++ templates/media/newest.php
@@ -100,13 +100,23 @@
         <?php $images = get_field('archive_gallery'); ?>
         <?php if( $images ): ?>
         <div class="event-gallery" itemscope itemtype="http://schema.org/ImageGallery">
-        <?php $counter = 1; foreach( $images as $image ): ?>
-        <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject" class="item_<?php echo $counter; ?>">
-        <a href="<?php echo $image['url']; ?>" itemprop="contentUrl" data-size="<?php echo $image['width']; ?>x<?php echo $image['height']; ?>">
-        <img src="<?php echo $image['sizes']['footer-module-image']; ?>" class="img-responsive" itemprop="thumbnail" alt="" />
-        </a>
+
+        <?php $image  = $images[0]; ?>
+
+        <?php if( $image ) : ?>
+        <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject" class="show item_<?php echo $counter; ?>">
+          <a href="<?php echo $image['url']; ?>" itemprop="contentUrl" data-size="<?php echo $image['width']; ?>x<?php echo $image['height']; ?>">
+            <img class="img-responsive" src="<?php echo $image['sizes']['footer-module-image']; ?>" alt="<?php echo $image['alt']; ?>" />
+          </a>
         </figure>
-        <?php  $counter++; endforeach; ?>
+        <?php endif; ?>
+
+        <?php $counter = 1; $i = 0; foreach( $images as $image ): $i++; if ($i != 1): ?>
+        <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject" class="hidden item_<?php echo $counter; ?>">
+        <a href="<?php echo $image['url']; ?>" itemprop="contentUrl" data-size="<?php echo $image['width']; ?>x<?php echo $image['height']; ?>"></a>
+        </figure>
+
+        <?php  $counter++; endif; endforeach; ?>
         </div>
         <?php endif; ?>
 
