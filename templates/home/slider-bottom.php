b8d96ae98fbc316222a7fc9070510095ad2aa14f
Craig Fowler <craig@wearegiants.com>
2015-05-30 18:00:28 -0700

    Stupid arrows

--- templates/home/slider-bottom.php
+++ templates/home/slider-bottom.php
@@ -32,15 +32,16 @@
 
 <div class="slide">
   <div class="content">
+    <a href="<?php the_permalink();?>">
     <div class="meta">
       <div class="row">
         <div class="desktop-10 centered">
           <?php if (!get_sub_field('custom_link')): ?>
-          <h2 class="title"><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
-          <a class="link right" href="<?php the_permalink();?>"><?php echo tribe_get_start_date($id, false, "l, M j, Y "); ?> | View Event</a>
+          <h2 class="title"><?php the_title(); ?></h2>
+          <?php echo tribe_get_start_date($id, false, "l, M j, Y "); ?> | View Event
           <?php else: ?>
-          <h2 class="title"><a href="<?php the_sub_field('url'); ?>"><?php the_sub_field('title'); ?></a></h2>
-          <a class="link right" href="<?php the_sub_field('url'); ?>"><?php the_sub_field('button_text'); ?></a>
+          <h2 class="title"><?php the_sub_field('title'); ?></h2>
+          <span class="link"><?php the_sub_field('button_text'); ?></span>
           <?php endif; ?>
         </div>
       </div>
@@ -48,15 +49,18 @@
     <div class="sub-meta">
       <div class="row">
         <div class="desktop-10 centered">
+
           <?php if (!get_sub_field('custom_link')): ?>
           <span class="sub_title_second"><?php the_field('event_title'); ?></span> |
           <span class="sub_title_second"><?php the_field('event_subtitle'); ?></span>
           <?php else: ?>
           <span class="sub_title_second"><?php the_sub_field('subtitle'); ?></span>
           <?php endif; ?>
+
         </div>
       </div>
     </div>
+    </a>
   </div>
   <a class="link right" href="<?php the_permalink();?>">
   <?php if (!get_sub_field('custom_link')): ?>
