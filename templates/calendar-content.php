4c03746679c8cf92d6222b963a522323ab0bf082
Craig Fowler <craig@wearegiants.com>
2015-05-30 17:25:07 -0700

    Store on wheels sidebar
    looks wonky

--- templates/calendar-content.php
+++ templates/calendar-content.php
@@ -20,16 +20,30 @@
 
 <div class="page-content">
   <div class="row">
-    <?php if ( is_tax() ) { ?>
+    <?php if ( is_tax() ): ?>
+
       <div class="<?php echo $pageWidth ;?>"><?php the_field('description',$taxonomy . '_' . $term_id); ?></div>
       <?php if ( get_field('include_aside',$taxonomy . '_' . $term_id)) { ?>
-      <aside class="aside desktop-4 tablet-2 mobile-3 right"><?php the_field('aside',$taxonomy . '_' . $term_id); ?></aside>
+      <aside class="aside desktop-4 tablet-2 mobile-3 right">
+        <?php the_field('aside',$taxonomy . '_' . $term_id); ?>
+
+        <?php // Pull in specific sidebars for categories ?>
+
+        <?php if ( is_tax('tribe_events_cat', 'library-store-on-wheels') ): ?>
+        <?php get_sidebar('simple'); ?>
+        <?php endif; ?>
+
+      </aside>
       <?php } ?>
-    <?php } else { ?>
+
+
+    <?php else: ?>
+
       <div class="<?php echo $pageWidth ;?>"><?php the_field('description','option'); ?></div>
       <?php if ( get_field('include_aside',$taxonomy . '_' . $term_id)) { ?>
       <aside class="aside desktop-4 tablet-2 mobile-3 right"><?php the_field('aside',$taxonomy . '_' . $term_id); ?></aside>
       <?php } ?>
-    <?php } ?>
+
+    <?php endif; ?>
   </div>
 </div>
