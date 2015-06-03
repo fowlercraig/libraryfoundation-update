db551c7a8475fa5de4601fbca5954d370b3807e1
Craig Fowler <craig@wearegiants.com>
2015-06-03 12:22:22 -0700

    added queries

--- functions.php
+++ functions.php
@@ -144,3 +144,7 @@ function child_manage_woocommerce_styles() {
 
 
 
+function wpse_footer_db_queries(){
+    echo '<!-- '.get_num_queries().' queries in '.timer_stop(0).' seconds. -->'.PHP_EOL;
+}
+add_action('wp_footer', 'wpse_footer_db_queries');
